48908bb0686a9ab832a1e693e9be43d42d80a2c4
reduviidae <wiljago@gmail.com>
2019-01-10 11:00:33 -0500

    controllers setup in config.ru

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,11 @@ class SongsController < ApplicationController
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
+    binding.pry
+  end
+
+  get '/songs/:id' do
+    puts "this is the show page"
   end
 
 end
